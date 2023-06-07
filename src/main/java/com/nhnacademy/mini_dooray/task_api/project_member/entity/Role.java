76353eb1006e25c414362cb116420a1fc69ecc27
O'mil <ghkwjd5343@gmail.com>
2023-06-07 13:09:04 +0900

    feat: 🎸 추가 Role Entity 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/entity/Role.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/entity/Role.java
@@ -1,2 +1,21 @@
-package com.nhnacademy.mini_dooray.task_api.project_member.entity;public class Role {
+package com.nhnacademy.mini_dooray.task_api.project_member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", nullable = false)
+    private Integer roleId;
+
+    @Column (name = "role_name", nullable = false)
+    private String roleName;
 }
