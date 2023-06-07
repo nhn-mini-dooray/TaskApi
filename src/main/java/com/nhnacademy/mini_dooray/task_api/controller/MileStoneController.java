ca84c04cedc7f16218fbb859e6b89b1c2771c8f0
O'mil <ghkwjd5343@gmail.com>
2023-06-07 16:05:11 +0900

    feat: 🎸 추가 milestone 조회를 위한 controller 추가

--- src/main/java/com/nhnacademy/mini_dooray/task_api/controller/MileStoneController.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/controller/MileStoneController.java
@@ -5,10 +5,7 @@ import com.nhnacademy.mini_dooray.task_api.service.MileStoneService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/milestones")
@@ -27,6 +24,18 @@ public class MileStoneController {
     public ResponseEntity<MileStoneDTO> createMileStone(@RequestBody MileStoneDTO mileStoneDTO) {
         MileStoneDTO createdMileStone = mileStoneService.createMileStone(mileStoneDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdMileStone);
+    }
+
+    /**
+     * 마일스톤 조회
+     *
+     * @param mileStoneId
+     * @return
+     */
+    @GetMapping("/{mileStoneId}")
+    public ResponseEntity<MileStoneDTO> getMileStone(@PathVariable Long mileStoneId) {
+        MileStoneDTO mileStoneDTO = mileStoneService.getMileStone(mileStoneId);
+        return ResponseEntity.ok(mileStoneDTO);
     }
 
 
