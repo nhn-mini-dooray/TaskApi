a039e13b2818f2e07343fc52c60825f6f25a8cad
O'mil <ghkwjd5343@gmail.com>
2023-06-07 13:14:41 +0900

    feat: 🎸 추가 ProjectStatusRepository 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/repository/ProjectStatusRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/repository/ProjectStatusRepository.java
@@ -1,2 +1,7 @@
-package com.nhnacademy.mini_dooray.task_api.project_member.repository;public interface ProjectStatusRepository {
+package com.nhnacademy.mini_dooray.task_api.project_member.repository;
+
+import com.nhnacademy.mini_dooray.task_api.project_member.entity.ProjectStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectStatusRepository extends JpaRepository<ProjectStatus, Integer> {
 }
