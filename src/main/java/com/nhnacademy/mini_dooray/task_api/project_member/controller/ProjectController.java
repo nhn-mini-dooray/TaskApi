24d17ca1c5267e97376decfce534aebda184b9d1
O'mil <ghkwjd5343@gmail.com>
2023-06-07 13:15:49 +0900

    feat: 🎸 추가 프로젝트 생성을 위한 Controller 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/controller/ProjectController.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/controller/ProjectController.java
@@ -1,2 +1,31 @@
-package com.nhnacademy.mini_dooray.task_api.project_member.controller;public class ProjectController {
+package com.nhnacademy.mini_dooray.task_api.project_member.controller;
+
+import com.nhnacademy.mini_dooray.task_api.project_member.dto.ProjectDTO;
+import com.nhnacademy.mini_dooray.task_api.project_member.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    /**
+     * 프로젝트 생성
+     *
+     * @param projectDTO
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<ProjectDTO> createProject(@RequestBody ProjectDTO projectDTO) {
+        ProjectDTO createdProject = projectService.createProject(projectDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProject);
+    }
 }
