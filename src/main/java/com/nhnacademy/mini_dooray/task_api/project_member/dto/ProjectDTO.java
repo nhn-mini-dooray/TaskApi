4d029b5608302d7bedb3f8e8a5cd1f36cc42a4a2
O'mil <ghkwjd5343@gmail.com>
2023-06-07 13:10:20 +0900

    feat: 🎸 추가 projectDTO 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/dto/ProjectDTO.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/dto/ProjectDTO.java
@@ -1,2 +1,13 @@
-package com.nhnacademy.mini_dooray.task_api.project_member.entity;public class projectDTO {
+package com.nhnacademy.mini_dooray.task_api.project_member.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProjectDTO {
+    private String projectName;
+    private Long accountId;
+    private Integer projectStatusId;
 }
