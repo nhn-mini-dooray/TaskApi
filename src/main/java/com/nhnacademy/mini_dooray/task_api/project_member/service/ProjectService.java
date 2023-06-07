fffade945abdf8d5ac7638905c9d67e565588ae9
O'mil <ghkwjd5343@gmail.com>
2023-06-07 13:15:19 +0900

    feat: 🎸 추가 프로젝트 생성을 위한 ProjectService 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/service/ProjectService.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/service/ProjectService.java
@@ -1,2 +1,34 @@
-package com.nhnacademy.mini_dooray.task_api.project_member.service;public class ProjectService {
+package com.nhnacademy.mini_dooray.task_api.project_member.service;
+
+import com.nhnacademy.mini_dooray.task_api.project_member.dto.ProjectDTO;
+import com.nhnacademy.mini_dooray.task_api.project_member.entity.Project;
+import com.nhnacademy.mini_dooray.task_api.project_member.entity.ProjectStatus;
+import com.nhnacademy.mini_dooray.task_api.project_member.repository.ProjectRepository;
+import com.nhnacademy.mini_dooray.task_api.project_member.repository.ProjectStatusRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+    private final ProjectStatusRepository projectStatusRepository;
+
+    /**
+     * 프로젝트 생성
+     *
+     * @param projectDTO
+     * @return
+     */
+    public ProjectDTO createProject(ProjectDTO projectDTO) {
+
+        ProjectStatus projectStatusId = projectStatusRepository.findById(projectDTO.getProjectStatusId()).orElse(null);
+
+        Project project = new Project(projectDTO.getProjectName(), projectDTO.getAccountId(), projectStatusId);
+
+        Project savedProject = projectRepository.save(project);
+
+        return new ProjectDTO(savedProject.getProjectName(), savedProject.getAccountId(),
+                savedProject.getProjectStatusId().getProjectStatusId());
+    }
 }
