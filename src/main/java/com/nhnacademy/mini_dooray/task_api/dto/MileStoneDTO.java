c88f3bb80ce4b24f902db904c91e63ed8f472a90
O'mil <ghkwjd5343@gmail.com>
2023-06-07 15:26:00 +0900

    feat: 🎸 추가 milestone CRUD를 위한 DTO 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/dto/MileStoneDTO.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/dto/MileStoneDTO.java
@@ -1,2 +1,16 @@
-package com.nhnacademy.mini_dooray.task_api.dto;public class MileStoneDTO {
+package com.nhnacademy.mini_dooray.task_api.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MileStoneDTO {
+    private Long projectId;
+    private String mileStoneName;
+    private LocalDate mileStoneStartDate;
+    private LocalDate mileStoneEndDate;
 }
