33f383511129bf05a4fbcd62b1501e077e9102e0
O'mil <ghkwjd5343@gmail.com>
2023-06-07 13:57:24 +0900

    feat: 🎸 추가 RoleRepository 생성

---
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.mini_dooray.task_api.repository;
+
+import com.nhnacademy.mini_dooray.task_api.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
