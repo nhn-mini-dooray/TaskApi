297bb8d59ff59542a2099341c359ace761c19b4b
O'mil <ghkwjd5343@gmail.com>
2023-06-07 13:06:01 +0900

    feat: 🎸 추가 Member Entity 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/entity/Member.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/entity/Member.java
@@ -1,2 +1,35 @@
-package com.nhnacademy.mini_dooray.task_api.project_member.entity;public class Member {
+package com.nhnacademy.mini_dooray.task_api.project_member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+@Table(name = "members")
+public class Member {
+
+    @EmbeddedId
+    private Pk pk;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public static class Pk implements Serializable {
+        @Column(name = "account_id")
+        private Long accountId;
+
+        @ManyToOne
+        @JoinColumn(name = "project_id", nullable = false)
+        private Project project;
+    }
 }
