569c872f2964673f82bc8ae7849a55db153ff4c7
O'mil <ghkwjd5343@gmail.com>
2023-06-07 14:24:44 +0900

    style: 💄 변경 파일 패키지 변경

--- src/main/java/com/nhnacademy/mini_dooray/task_api/repository/ProjectRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/repository/ProjectRepository.java
@@ -1,6 +1,6 @@
-package com.nhnacademy.mini_dooray.task_api.project_member.repository;
+package com.nhnacademy.mini_dooray.task_api.repository;
 
-import com.nhnacademy.mini_dooray.task_api.project_member.entity.Project;
+import com.nhnacademy.mini_dooray.task_api.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
