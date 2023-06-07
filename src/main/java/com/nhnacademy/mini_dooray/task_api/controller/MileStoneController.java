8062fe1d1718f6adeedb36e0db9a692d137625e9
O'mil <ghkwjd5343@gmail.com>
2023-06-07 15:28:30 +0900

    feat: 🎸 추가 milestone 생성을 위한 controller 추가

--- src/main/java/com/nhnacademy/mini_dooray/task_api/controller/MileStoneController.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/controller/MileStoneController.java
@@ -1,2 +1,25 @@
-package com.nhnacademy.mini_dooray.task_api.controller;public class MileStoneController {
+package com.nhnacademy.mini_dooray.task_api.controller;
+
+import com.nhnacademy.mini_dooray.task_api.dto.MileStoneDTO;
+import com.nhnacademy.mini_dooray.task_api.service.MileStoneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/milestones")
+@RequiredArgsConstructor
+public class MileStoneController {
+
+    private final MileStoneService mileStoneService;
+
+    @PostMapping
+    public ResponseEntity<MileStoneDTO> createMileStone(@RequestBody MileStoneDTO mileStoneDTO) {
+        MileStoneDTO createdMileStone = mileStoneService.createMileStone(mileStoneDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdMileStone);
+    }
 }
