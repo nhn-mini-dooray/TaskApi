64fb67f94f8b5884891a7a7ab23c7ab372388520
O'mil <ghkwjd5343@gmail.com>
2023-06-07 13:37:33 +0900

    feat: 🎸 추가 프로젝트생태 수정 DTO 추가

---
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/dto/ProjectStatusModifyDTO.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.mini_dooray.task_api.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProjectStatusModifyDTO {
+    private Integer projectStatusId;
+}
