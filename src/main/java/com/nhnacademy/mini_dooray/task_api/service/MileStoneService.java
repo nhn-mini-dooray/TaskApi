62708672107a1499d259e9d2357b9ec561da5faf
O'mil <ghkwjd5343@gmail.com>
2023-06-07 15:27:30 +0900

    feat: 🎸 추가 milestone 생성을 위한 service 추가

--- src/main/java/com/nhnacademy/mini_dooray/task_api/service/MileStoneService.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/service/MileStoneService.java
@@ -1,2 +1,35 @@
-package com.nhnacademy.mini_dooray.task_api.service;public class MileStoneService {
+package com.nhnacademy.mini_dooray.task_api.service;
+
+import com.nhnacademy.mini_dooray.task_api.dto.MileStoneDTO;
+import com.nhnacademy.mini_dooray.task_api.entity.MileStone;
+import com.nhnacademy.mini_dooray.task_api.entity.Project;
+import com.nhnacademy.mini_dooray.task_api.repository.MileSoneRepository;
+import com.nhnacademy.mini_dooray.task_api.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MileStoneService {
+
+    private final MileSoneRepository mileStoneRepository;
+    private final ProjectRepository projectRepository;
+
+    /**
+     * 마일스톤 생성
+     *
+     * @param mileStoneDTO
+     * @return
+     */
+    public MileStoneDTO createMileStone(MileStoneDTO mileStoneDTO) {
+        Project projectId = projectRepository.findById(mileStoneDTO.getProjectId())
+                .orElseThrow(() -> new IllegalArgumentException("Project Not Found"));
+
+        MileStone mileStone = new MileStone(projectId, mileStoneDTO.getMileStoneName(), mileStoneDTO.getMileStoneStartDate(), mileStoneDTO.getMileStoneEndDate());
+
+        MileStone savedMileStone = mileStoneRepository.save(mileStone);
+
+        return new MileStoneDTO(savedMileStone.getProjectId().getProjectId(),
+                savedMileStone.getMileStoneName(), savedMileStone.getMileStoneStartDate(), savedMileStone.getMileStoneEndDate());
+    }
 }
