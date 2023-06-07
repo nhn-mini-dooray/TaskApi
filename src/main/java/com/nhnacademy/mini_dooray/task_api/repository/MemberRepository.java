ece8a4121a96fb57a4c4e3d65f83aa7cecd15013
O'mil <ghkwjd5343@gmail.com>
2023-06-07 14:06:32 +0900

    feat: 🎸 추가 멤버 아이디로 프로젝트 조회를 위한 JPA 메서드 추가

--- src/main/java/com/nhnacademy/mini_dooray/task_api/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.nhnacademy.mini_dooray.task_api.repository;
 import com.nhnacademy.mini_dooray.task_api.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Member.Pk> {
+    List<Member> findByPkAccountId(Long accountId);
+
 }
