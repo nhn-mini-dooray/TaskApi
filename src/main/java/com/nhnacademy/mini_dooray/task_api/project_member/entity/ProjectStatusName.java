a6519727171ff067478e8422065bd344bf46eb42
O'mil <ghkwjd5343@gmail.com>
2023-06-07 13:07:29 +0900

    feat: 🎸 추가 Project Status Name Enum 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/entity/ProjectStatusName.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/project_member/entity/ProjectStatusName.java
@@ -1,2 +1,13 @@
-package com.nhnacademy.mini_dooray.task_api.project_member.entity;public class ProjectStatusNAme {
+package com.nhnacademy.mini_dooray.task_api.project_member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@RequiredArgsConstructor
+public enum ProjectStatusName {
+    ACTIVE("활성"),
+    INACTIVE("휴먼"),
+    COMPLETE("종료");
+
+    private final String description;
 }
