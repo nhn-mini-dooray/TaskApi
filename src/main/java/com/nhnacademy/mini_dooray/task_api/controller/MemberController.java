625dc64ba9930b528cbed26edea1e097cc39f080
O'mil <ghkwjd5343@gmail.com>
2023-06-07 13:59:21 +0900

    feat: 🎸 추가 멤버 추가를 위한 Controller 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/controller/MemberController.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/controller/MemberController.java
@@ -1,2 +1,31 @@
-package com.nhnacademy.mini_dooray.task_api.controller;public class MemberController {
+package com.nhnacademy.mini_dooray.task_api.controller;
+
+import com.nhnacademy.mini_dooray.task_api.dto.MemberDTO;
+import com.nhnacademy.mini_dooray.task_api.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/project/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    /**
+     * 멤버 추가
+     *
+     * @param memberDTO
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<MemberDTO> createMember(@RequestBody MemberDTO memberDTO) {
+        MemberDTO createdMember = memberService.createMember(memberDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdMember);
+    }
 }
