a9513d3723a31c17dc12e800492d395e9bdb5d41
O'mil <ghkwjd5343@gmail.com>
2023-06-09 13:06:16 +0900

    refactor: 💡 변경 created 사용, 필요한 파라미터를 넣는 형식으로 작성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/controller/MemberController.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/controller/MemberController.java
@@ -4,7 +4,6 @@ import com.nhnacademy.mini_dooray.task_api.dto.MemberDTO;
 import com.nhnacademy.mini_dooray.task_api.dto.ProjectDTO;
 import com.nhnacademy.mini_dooray.task_api.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
