21d7ba342fe423b64b3b77994a737693b9e6219d
O'mil <ghkwjd5343@gmail.com>
2023-06-09 13:48:52 +0900

    refactor: 💡 변경 created에 uri 사용

--- src/main/java/com/nhnacademy/mini_dooray/task_api/controller/ProjectController.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/controller/ProjectController.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,7 +30,10 @@ public class ProjectController {
     @PostMapping
     public ResponseEntity<ProjectDTO> createProject(@RequestBody ProjectDTO projectDTO) {
         ProjectDTO createdProject = projectService.createProject(projectDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdProject);
+        URI location = UriComponentsBuilder.fromPath("/{id}")
+                .buildAndExpand(createdProject.getProjectName())
+                .toUri();
+        return ResponseEntity.created(location).body(createdProject);
     }
 
     /**
