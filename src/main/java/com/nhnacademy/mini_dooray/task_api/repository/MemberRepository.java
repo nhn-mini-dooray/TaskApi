06e4d82da90b38c7fd1f262142b3f0eb6034d44f
O'mil <ghkwjd5343@gmail.com>
2023-06-09 17:35:46 +0900

    refactor: 💡 삭제 findByPkAccountId 메서드 삭제

--- src/main/java/com/nhnacademy/mini_dooray/task_api/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/repository/MemberRepository.java
@@ -3,9 +3,5 @@ package com.nhnacademy.mini_dooray.task_api.repository;
 import com.nhnacademy.mini_dooray.task_api.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MemberRepository extends JpaRepository<Member, Member.Pk> {
-    List<Member> findByPkAccountId(Long accountId);
-
 }
