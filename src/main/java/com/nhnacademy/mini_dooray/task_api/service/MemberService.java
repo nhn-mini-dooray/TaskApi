a358017c21ccbdcff180008b5b63a5bbd64694db
O'mil <ghkwjd5343@gmail.com>
2023-06-09 17:21:21 +0900

    Revert "refactor: 💡 변경 멤버 추가 service 코드 queryDSL로 변경"
    This reverts commit 39f1c933951348934ab738663a355a546feb832f.
    
    l

--- src/main/java/com/nhnacademy/mini_dooray/task_api/service/MemberService.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/service/MemberService.java
@@ -4,17 +4,13 @@ import com.nhnacademy.mini_dooray.task_api.dto.MemberDTO;
 import com.nhnacademy.mini_dooray.task_api.dto.ProjectDTO;
 import com.nhnacademy.mini_dooray.task_api.entity.Member;
 import com.nhnacademy.mini_dooray.task_api.entity.Project;
-import com.nhnacademy.mini_dooray.task_api.entity.QMember;
-import com.nhnacademy.mini_dooray.task_api.entity.QProject;
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.nhnacademy.mini_dooray.task_api.repository.MemberRepository;
 import com.nhnacademy.mini_dooray.task_api.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,8 +19,6 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final ProjectRepository projectRepository;
-    private final JPAQueryFactory jpaQueryFactory;
-
     /**
      * 멤버 추가
      *
@@ -55,20 +49,20 @@ public class MemberService {
      * @return
      */
     public List<ProjectDTO> getProjectForMember(Long accountId) {
-        QMember member = QMember.member;
-        QProject project = QProject.project;
+        List<Member> members = memberRepository.findByPkAccountId(accountId);
 
-        return jpaQueryFactory
-                .select(Projections.constructor(ProjectDTO.class,
-                        project.projectName,
-                        member.pk.accountId,
-                        project.projectStatusId.projectStatusId))
-                .from(member)
-                .join(member.pk.project, project)
-                .where(member.pk.accountId.eq(accountId))
-                .fetch();
+        List<ProjectDTO> projectDTOs = new ArrayList<>();
+        for (Member member: members) {
+            Project project = member.getPk().getProject();
+            ProjectDTO projectDTO = new ProjectDTO(
+                    project.getProjectName(),
+                    member.getPk().getAccountId(),
+                    project.getProjectStatusId().getProjectStatusId()
+            );
+            projectDTOs.add(projectDTO);
+        }
+        return projectDTOs;
     }
-
 
     /**
      * 멤버 삭제
