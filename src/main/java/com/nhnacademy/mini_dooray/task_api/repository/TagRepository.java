321ab7521443e098758bde393efbcafdd34038f8
O'mil <ghkwjd5343@gmail.com>
2023-06-11 11:35:52 +0900

    feat: 🎸 추가 태그 Repository 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/repository/TagRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/repository/TagRepository.java
@@ -1,2 +1,9 @@
-package com.nhnacademy.mini_dooray.task_api.repository;public interface TagRepository {
+package com.nhnacademy.mini_dooray.task_api.repository;
+
+import com.nhnacademy.mini_dooray.task_api.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
 }
