8a407193bf3073d51ad36a22ded573ec494df685
O'mil <ghkwjd5343@gmail.com>
2023-06-11 13:56:42 +0900

    feat: 🎸 추가 project에 등록된 Tag 조회 service 추가

--- src/main/java/com/nhnacademy/mini_dooray/task_api/service/TagService.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/service/TagService.java
@@ -2,11 +2,16 @@ package com.nhnacademy.mini_dooray.task_api.service;
 
 import com.nhnacademy.mini_dooray.task_api.dto.TagDTO;
 import com.nhnacademy.mini_dooray.task_api.entity.Project;
+import com.nhnacademy.mini_dooray.task_api.entity.QTag;
 import com.nhnacademy.mini_dooray.task_api.entity.Tag;
 import com.nhnacademy.mini_dooray.task_api.repository.ProjectRepository;
 import com.nhnacademy.mini_dooray.task_api.repository.TagRepository;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +19,7 @@ public class TagService {
 
     private final TagRepository tagRepository;
     private final ProjectRepository projectRepository;
+    private final JPAQueryFactory jpaQueryFactory;
 
     /**
      * Tag 생성
@@ -30,5 +36,23 @@ public class TagService {
         Tag savedTag = tagRepository.save(tag);
 
         return new TagDTO(savedTag.getProjectId().getProjectId(),savedTag.getTagName());
+    }
+
+    /**
+     * projectId로 Tag 조회
+     *
+     * @param projectId
+     * @return
+     */
+    public List<TagDTO> getTagsByProjectId(Long projectId) {
+        QTag tag = QTag.tag;
+
+        return jpaQueryFactory
+                .select(Projections.constructor(TagDTO.class,
+                        tag.projectId.projectId,
+                        tag.tagName))
+                .from(tag)
+                .where(tag.projectId.projectId.eq(projectId))
+                .fetch();
     }
 }
