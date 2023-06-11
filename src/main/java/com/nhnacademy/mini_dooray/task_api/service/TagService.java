1fd27e8ae0459b4d01636b10d7006607c7ba34f7
O'mil <ghkwjd5343@gmail.com>
2023-06-11 11:36:33 +0900

    feat: 🎸 추가 태그 등록을 위한 태그 Service 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/service/TagService.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/service/TagService.java
@@ -1,2 +1,34 @@
-package com.nhnacademy.mini_dooray.task_api.service;public class TagService {
+package com.nhnacademy.mini_dooray.task_api.service;
+
+import com.nhnacademy.mini_dooray.task_api.dto.TagDTO;
+import com.nhnacademy.mini_dooray.task_api.entity.Project;
+import com.nhnacademy.mini_dooray.task_api.entity.Tag;
+import com.nhnacademy.mini_dooray.task_api.repository.ProjectRepository;
+import com.nhnacademy.mini_dooray.task_api.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TagService {
+
+    private final TagRepository tagRepository;
+    private final ProjectRepository projectRepository;
+
+    /**
+     * Tag 생성
+     *
+     * @param tagDTO
+     * @return
+     */
+    public TagDTO createTag(TagDTO tagDTO) {
+
+        Project projectId = projectRepository.findById(tagDTO.getProjectId())
+                .orElseThrow(() -> new IllegalArgumentException("projectId Not Found"));
+        Tag tag = new Tag(projectId, tagDTO.getTagName());
+
+        Tag savedTag = tagRepository.save(tag);
+
+        return new TagDTO(savedTag.getProjectId().getProjectId(),savedTag.getTagName());
+    }
 }
