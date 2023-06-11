9002155cc57b9a6098aafaef62ad1417895f4526
O'mil <ghkwjd5343@gmail.com>
2023-06-11 11:37:15 +0900

    feat: 🎸 추가 태그 생성을 위한 태그 Controller 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/controller/TagController.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/controller/TagController.java
@@ -1,2 +1,34 @@
-package com.nhnacademy.mini_dooray.task_api.controller;public class TagController {
+package com.nhnacademy.mini_dooray.task_api.controller;
+
+import com.nhnacademy.mini_dooray.task_api.dto.TagDTO;
+import com.nhnacademy.mini_dooray.task_api.service.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/tags")
+@RequiredArgsConstructor
+public class TagController {
+    private final TagService tagService;
+
+    /**
+     * Tag 생성
+     * @param tagDTO
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<TagDTO> createTag(@RequestBody TagDTO tagDTO) {
+        TagDTO createdTag = tagService.createTag(tagDTO);
+        URI locaion = UriComponentsBuilder.fromPath("/{tagId}")
+                .buildAndExpand(createdTag.getTagName())
+                .toUri();
+        return ResponseEntity.created(locaion).body(createdTag);
+    }
 }
