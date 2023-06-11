00dae26e6015b9760471b26ff46dfaebc964a721
O'mil <ghkwjd5343@gmail.com>
2023-06-11 11:35:05 +0900

    feat: 🎸 추가 태그 DTO 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/dto/TagDTO.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/dto/TagDTO.java
@@ -1,2 +1,12 @@
-package com.nhnacademy.mini_dooray.task_api.dto;public class TagDTO {
+package com.nhnacademy.mini_dooray.task_api.dto;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TagDTO {
+
+    private Long projectId;
+    private String tagName;
 }
