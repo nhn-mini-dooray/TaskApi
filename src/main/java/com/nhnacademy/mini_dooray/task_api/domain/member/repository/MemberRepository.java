225703f512cfa39d1fe40e5f6bcb8a42d6a70a64
O'mil <ghkwjd5343@gmail.com>
2023-06-12 17:43:01 +0900

    style: 💄 변경 폴더 이동

--- src/main/java/com/nhnacademy/mini_dooray/task_api/domain/member/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/domain/member/repository/MemberRepository.java
@@ -1,6 +1,6 @@
-package com.nhnacademy.mini_dooray.task_api.repository;
+package com.nhnacademy.mini_dooray.task_api.domain.member.repository;
 
-import com.nhnacademy.mini_dooray.task_api.entity.Member;
+import com.nhnacademy.mini_dooray.task_api.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Member.Pk> {
