e3e16ca5639f2174a393588758b510e97a5a40b9
O'mil <ghkwjd5343@gmail.com>
2023-06-12 17:47:43 +0900

    style: 💄 변경 폴더 이동

--- src/main/java/com/nhnacademy/mini_dooray/task_api/domain/milestone/repository/MileStoneRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/domain/milestone/repository/MileStoneRepository.java
@@ -3,5 +3,5 @@ package com.nhnacademy.mini_dooray.task_api.domain.milestone.repository;
 import com.nhnacademy.mini_dooray.task_api.domain.milestone.entity.MileStone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MileSoneRepository extends JpaRepository<MileStone, Long> {
+public interface MileStoneRepository extends JpaRepository<MileStone, Long> {
 }
