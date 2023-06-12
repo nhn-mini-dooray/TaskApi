d47ffd8568db68e94180e6b3f740fe84e61a9b04
O'mil <ghkwjd5343@gmail.com>
2023-06-12 16:43:00 +0900

    feat: 🎸 추가 Task CRUD Service 생성

--- src/main/java/com/nhnacademy/mini_dooray/task_api/domain/tasks/service/TaskService.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/domain/tasks/service/TaskService.java
@@ -1,48 +1,76 @@
-package com.nhnacademy.mini_dooray.account_api.domain.tasks.service;
+package com.nhnacademy.mini_dooray.task_api.domain.tasks.service;
 
-import com.nhnacademy.mini_dooray.account_api.domain.mile_stones.entity.MileStone;
-import com.nhnacademy.mini_dooray.account_api.domain.mile_stones.repository.MileStoneRepository;
-import com.nhnacademy.mini_dooray.account_api.domain.project.entity.Project;
-import com.nhnacademy.mini_dooray.account_api.domain.project.repository.ProjectRepository;
-import com.nhnacademy.mini_dooray.account_api.domain.tasks.entity.Task;
-import com.nhnacademy.mini_dooray.account_api.domain.tasks.model.request.CreateTaskRequestDto;
-import com.nhnacademy.mini_dooray.account_api.domain.tasks.model.request.UpdateTaskRequestDto;
-import com.nhnacademy.mini_dooray.account_api.domain.tasks.model.response.TaskResponseDto;
-import com.nhnacademy.mini_dooray.account_api.domain.tasks.repository.TaskRepository;
-import com.nhnacademy.mini_dooray.account_api.exception.NotFoundException;
+import com.nhnacademy.mini_dooray.task_api.domain.comments.repository.CommentRepository;
+import com.nhnacademy.mini_dooray.task_api.domain.milestone.entity.MileStone;
+import com.nhnacademy.mini_dooray.task_api.domain.milestone.repository.MileStoneRepository;
+import com.nhnacademy.mini_dooray.task_api.domain.project.entity.Project;
+import com.nhnacademy.mini_dooray.task_api.domain.project.repository.ProjectRepository;
+import com.nhnacademy.mini_dooray.task_api.domain.task_tag.repository.TaskTagRepository;
+import com.nhnacademy.mini_dooray.task_api.domain.tasks.entity.Task;
+import com.nhnacademy.mini_dooray.task_api.domain.tasks.model.request.CreateTaskRequestDto;
+import com.nhnacademy.mini_dooray.task_api.domain.tasks.model.request.UpdateTaskRequestDto;
+import com.nhnacademy.mini_dooray.task_api.domain.tasks.model.response.TaskResponseDto;
+import com.nhnacademy.mini_dooray.task_api.domain.tasks.repository.TaskRepository;
+import com.nhnacademy.mini_dooray.task_api.exception.NotFoundException;
+import com.nhnacademy.mini_dooray.task_api.domain.tasks.model.request.UpdateTaskRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.Valid;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class TaskService {
 
+    private final CommentRepository commentRepository;
+    private final TaskTagRepository taskTagRepository;
     private final TaskRepository taskRepository;
     private final ProjectRepository projectRepository;
     private final MileStoneRepository mileStoneRepository;
 
-    public TaskResponseDto select(Long id){
+    /**
+     * Task 조회
+     *
+     * @param id
+     * @return
+     */
+    public TaskResponseDto select(Long id) {
         Task task = taskRepository.findById(id).orElseThrow(NotFoundException::new);
         return TaskResponseDto.fromEntity(task);
     }
 
-    public void create(CreateTaskRequestDto requestDto){
+    /**
+     * Task 생성
+     *
+     * @param requestDto
+     */
+    public void create(CreateTaskRequestDto requestDto) {
         Project project = projectRepository.findById(requestDto.getProjectId()).orElseThrow(NotFoundException::new);
         MileStone mileStone = mileStoneRepository.findById(requestDto.getMileStoneId()).orElseThrow(NotFoundException::new);
-        taskRepository.save(CreateTaskRequestDto.toEntity(requestDto,project,mileStone));
+        taskRepository.save(CreateTaskRequestDto.toEntity(requestDto, project, mileStone));
 
     }
 
-    public void update(Long id, UpdateTaskRequestDto requestDto){
+    /**
+     * Task 수정
+     *
+     * @param id
+     * @param requestDto
+     */
+    public void update(Long id, UpdateTaskRequestDto requestDto) {
         Task task = taskRepository.findById(id).orElseThrow(NotFoundException::new);
         task.setTaskName(requestDto.getName());
         task.setTaskContent(requestDto.getContent());
     }
 
-    public void delete(Long id){
+    /**
+     * Task 삭제
+     *
+     * @param id
+     */
+    public void delete(Long id) {
         taskRepository.deleteById(id);
     }
-
 }
