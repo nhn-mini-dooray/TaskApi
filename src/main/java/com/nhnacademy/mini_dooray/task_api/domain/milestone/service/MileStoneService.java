8fbad40067fa3e90e9705b600eaf308e47f5d428
O'mil <ghkwjd5343@gmail.com>
2023-06-12 17:48:27 +0900

    style: 💄 변경 폴더 이동

--- src/main/java/com/nhnacademy/mini_dooray/task_api/domain/milestone/service/MileStoneService.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/domain/milestone/service/MileStoneService.java
@@ -1,10 +1,10 @@
-package com.nhnacademy.mini_dooray.task_api.service;
+package com.nhnacademy.mini_dooray.task_api.domain.milestone.service;
 
-import com.nhnacademy.mini_dooray.task_api.dto.MileStoneDTO;
-import com.nhnacademy.mini_dooray.task_api.entity.MileStone;
-import com.nhnacademy.mini_dooray.task_api.entity.Project;
-import com.nhnacademy.mini_dooray.task_api.repository.MileSoneRepository;
-import com.nhnacademy.mini_dooray.task_api.repository.ProjectRepository;
+import com.nhnacademy.mini_dooray.task_api.domain.milestone.model.MileStoneDTO;
+import com.nhnacademy.mini_dooray.task_api.domain.milestone.entity.MileStone;
+import com.nhnacademy.mini_dooray.task_api.domain.project.entity.Project;
+import com.nhnacademy.mini_dooray.task_api.domain.milestone.repository.MileStoneRepository;
+import com.nhnacademy.mini_dooray.task_api.domain.project.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MileStoneService {
 
-    private final MileSoneRepository mileStoneRepository;
+    private final MileStoneRepository mileStoneRepository;
     private final ProjectRepository projectRepository;
 
     /**
@@ -28,9 +28,10 @@ public class MileStoneService {
         MileStone mileStone = new MileStone(projectId, mileStoneDTO.getMileStoneName(), mileStoneDTO.getMileStoneStartDate(), mileStoneDTO.getMileStoneEndDate());
 
         MileStone savedMileStone = mileStoneRepository.save(mileStone);
-
-        return new MileStoneDTO(savedMileStone.getProjectId().getProjectId(),
-                savedMileStone.getMileStoneName(), savedMileStone.getMileStoneStartDate(), savedMileStone.getMileStoneEndDate());
+//
+//        return new MileStoneDTO(savedMileStone.getProjectId().getProjectId(),
+//                savedMileStone.getMileStoneName(), savedMileStone.getMileStoneStartDate(), savedMileStone.getMileStoneEndDate());
+        return null;
     }
 
     /**
@@ -43,12 +44,13 @@ public class MileStoneService {
         MileStone mileStone = mileStoneRepository.findById(mileStoneId)
                 .orElseThrow(() -> new IllegalArgumentException("MileStone Not Found"));
 
-        return new MileStoneDTO(
-                mileStone.getProjectId().getProjectId(),
-                mileStone.getMileStoneName(),
-                mileStone.getMileStoneStartDate(),
-                mileStone.getMileStoneEndDate()
-        );
+//        return new MileStoneDTO(
+//                mileStone.getProjectId().getProjectId(),
+//                mileStone.getMileStoneName(),
+//                mileStone.getMileStoneStartDate(),
+//                mileStone.getMileStoneEndDate()
+//        );
+        return null;
     }
 
     /**
@@ -62,24 +64,25 @@ public class MileStoneService {
         MileStone mileStone = mileStoneRepository.findById(mileStoneId)
                 .orElseThrow(() -> new IllegalArgumentException("MileStone Not Found"));
 
-        if (mileStoneDTO.getMileStoneName() != null) {
-            mileStone.setMileStoneName(mileStoneDTO.getMileStoneName());
-        }
-        if (mileStoneDTO.getMileStoneStartDate() != null) {
-            mileStone.setMileStoneStartDate(mileStoneDTO.getMileStoneStartDate());
-        }
-        if (mileStoneDTO.getMileStoneEndDate() != null) {
-            mileStone.setMileStoneEndDate(mileStoneDTO.getMileStoneEndDate());
-        }
-
-        MileStone updatedMileStone = mileStoneRepository.save(mileStone);
-
-        return new MileStoneDTO(
-                updatedMileStone.getProjectId().getProjectId(),
-                updatedMileStone.getMileStoneName(),
-                updatedMileStone.getMileStoneStartDate(),
-                updatedMileStone.getMileStoneEndDate()
-        );
+//        if (mileStoneDTO.getMileStoneName() != null) {
+//            mileStone.setMileStoneName(mileStoneDTO.getMileStoneName());
+//        }
+//        if (mileStoneDTO.getMileStoneStartDate() != null) {
+//            mileStone.setMileStoneStartDate(mileStoneDTO.getMileStoneStartDate());
+//        }
+//        if (mileStoneDTO.getMileStoneEndDate() != null) {
+//            mileStone.setMileStoneEndDate(mileStoneDTO.getMileStoneEndDate());
+//        }
+//
+//        MileStone updatedMileStone = mileStoneRepository.save(mileStone);
+//
+//        return new MileStoneDTO(
+//                updatedMileStone.getProjectId().getProjectId(),
+//                updatedMileStone.getMileStoneName(),
+//                updatedMileStone.getMileStoneStartDate(),
+//                updatedMileStone.getMileStoneEndDate()
+//        );
+        return null;
     }
 
     /**
