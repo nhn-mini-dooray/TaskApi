7a7eeb4bee2290b80c1f90271b93b8ab53a794a7
O'mil <ghkwjd5343@gmail.com>
2023-06-12 16:34:43 +0900

    feat: 🎸 추가 TaskTag Repository 추가

--- src/main/java/com/nhnacademy/mini_dooray/task_api/domain/task_tag/repository/TaskTagRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/domain/task_tag/repository/TaskTagRepository.java
@@ -1,7 +1,6 @@
-package com.nhnacademy.mini_dooray.account_api.domain.task_tag.repository;
+package com.nhnacademy.mini_dooray.task_api.domain.task_tag.repository;
 
-import com.nhnacademy.mini_dooray.account_api.domain.tags.entity.Tag;
-import com.nhnacademy.mini_dooray.account_api.domain.task_tag.entity.TaskTag;
+import com.nhnacademy.mini_dooray.task_api.domain.task_tag.entity.TaskTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskTagRepository extends JpaRepository<TaskTag, TaskTag.Pk> {
