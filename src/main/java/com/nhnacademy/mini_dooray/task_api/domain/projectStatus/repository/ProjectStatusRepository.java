6277a72aaa4835a48006b241983d09052a4043f8
O'mil <ghkwjd5343@gmail.com>
2023-06-12 18:04:11 +0900

    style: 💄 변경 폴더 이동

--- src/main/java/com/nhnacademy/mini_dooray/task_api/domain/projectStatus/repository/ProjectStatusRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/domain/projectStatus/repository/ProjectStatusRepository.java
@@ -1,6 +1,6 @@
-package com.nhnacademy.mini_dooray.task_api.repository;
+package com.nhnacademy.mini_dooray.task_api.domain.projectStatus.repository;
 
-import com.nhnacademy.mini_dooray.task_api.entity.ProjectStatus;
+import com.nhnacademy.mini_dooray.task_api.domain.projectStatus.entity.ProjectStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectStatusRepository extends JpaRepository<ProjectStatus, Integer> {
