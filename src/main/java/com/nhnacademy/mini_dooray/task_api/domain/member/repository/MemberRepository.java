8b7d1b8b4bed299aed857c8c0386ae7ee22acf02
O'mil <ghkwjd5343@gmail.com>
2023-06-14 13:28:34 +0900

    refactor: 💡 변경 MemberRepositoryCustom 인터페이스 추가

--- src/main/java/com/nhnacademy/mini_dooray/task_api/domain/member/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/domain/member/repository/MemberRepository.java
@@ -3,5 +3,5 @@ package com.nhnacademy.mini_dooray.task_api.domain.member.repository;
 import com.nhnacademy.mini_dooray.task_api.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Member.Pk> {
+public interface MemberRepository extends JpaRepository<Member, Member.Pk>, MemberRepositoryCustom {
 }
