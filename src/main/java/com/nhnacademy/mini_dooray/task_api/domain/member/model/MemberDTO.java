392288575305e06e9892ab1598aadc1b3220b0a3
O'mil <ghkwjd5343@gmail.com>
2023-06-14 10:57:56 +0900

    refactor: 💡 삭제 Setter 어노테이션 삭제

--- src/main/java/com/nhnacademy/mini_dooray/task_api/domain/member/model/MemberDTO.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/domain/member/model/MemberDTO.java
@@ -3,7 +3,6 @@ package com.nhnacademy.mini_dooray.task_api.domain.member.model;
 import lombok.*;
 
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberDTO {
