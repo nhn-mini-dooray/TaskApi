bd4a59b2c3cba5166fa98895807b0f69747373e1
O'mil <ghkwjd5343@gmail.com>
2023-06-14 10:36:35 +0900

    refactor: 💡 변경 ProjectRepositoryCustom 인터페이스 추가

--- src/main/java/com/nhnacademy/mini_dooray/task_api/domain/project/repository/ProjectRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/domain/project/repository/ProjectRepository.java
@@ -3,5 +3,5 @@ package com.nhnacademy.mini_dooray.task_api.domain.project.repository;
 import com.nhnacademy.mini_dooray.task_api.domain.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectRepositoryCustom {
 }
