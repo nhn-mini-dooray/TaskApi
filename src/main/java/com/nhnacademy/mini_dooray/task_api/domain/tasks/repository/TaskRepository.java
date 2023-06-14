5d478754bc33329323dfab0bbeb16dd33ff8bca2
O'mil <ghkwjd5343@gmail.com>
2023-06-14 14:45:19 +0900

    feat: 🎸 추가 findByProject 추가

--- src/main/java/com/nhnacademy/mini_dooray/task_api/domain/tasks/repository/TaskRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/task_api/domain/tasks/repository/TaskRepository.java
@@ -1,7 +1,11 @@
 package com.nhnacademy.mini_dooray.task_api.domain.tasks.repository;
 
+import com.nhnacademy.mini_dooray.task_api.domain.project.entity.Project;
 import com.nhnacademy.mini_dooray.task_api.domain.tasks.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByProject(Project project);
 }
